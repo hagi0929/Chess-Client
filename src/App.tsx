8323be5dcf2f11fc379474892f28bfb00fb6f5bc
Jaehak Kim <ministove3yo@gmail.com>
2023-02-18 13:10:15 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  ws: any;
+
+  componentDidMount() {
+    this.ws = new WebSocket("ws://127.0.0.1:8080");
+    this.ws.onopen = () => {   // 연결!
+      console.log("connected!!");
+    };
+  }
+  sendMessage = () => {  // 화살표함수로 만들것!!
+    this.ws.send("hello this is client Message");  // 서버로 메세지 보내는건 send
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <button onClick={this.sendMessage}>메세지 보내기</button>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
